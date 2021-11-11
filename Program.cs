77b751a9ac95c53ebacbb372af7a3de19684881e
Lavanya9493 <lavanyajonnalagadda123@gmail.com>
2021-11-11 15:36:33 +0530

    Interface

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+interface I1
+{
+    void InterfaceMethod();
+}
+interface I2
+{
+    void InterfaceMethod();
+}
+class Myclass:I1,I2
+{
+    public void InterfaceMethod()
+    {
+       Console.WriteLine("I1 InterfaceMethod");
+    }
+    void I2.InterfaceMethod()
+    {
+        Console.WriteLine("I2 InterfaceMethod");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Myclass c=new Myclass();
+        c.InterfaceMethod();
+        I2 i2=c;
+        i2.InterfaceMethod();
+    }
+}
